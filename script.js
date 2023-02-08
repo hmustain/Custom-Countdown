bc2764d1003b5edd9bffc233c7de54e9d938c879
hmustain <hunter@bunkerbranding.com>
2023-02-07 21:47:25 -0600

    fixed a bug where form wasn't clearing upon reset

--- script.js
+++ script.js
@@ -7,11 +7,16 @@ const countdownElTitle = document.getElementById('countdown-title');
 const countdownBtn = document.getElementById('countdown-button');
 const timeElements = document.querySelectorAll('span');
 
+const completeEl = document.getElementById('complete');
+const completeElInfo = document.getElementById('complete-info');
+const completeBtn = document.getElementById('complete-button');
+
+
 
 let countdownTitle = "";
 let countdownDate = "";
 let countdownValue = Date;
-let countdownActive
+let countdownActive;
 
 // define time
 const second = 1000
@@ -27,51 +32,82 @@ dateEl.setAttribute('min', today);
 
 // function for countdown
 function startCountdown() {
+  activeCountdown = setInterval(() => {
     const now = new Date().getTime()
     const distance = countdownValue - now
     console.log('distance :', distance);
 
     // define time
-    const days = Math.floor( distance / day)
+    const days = Math.floor(distance / day)
     const hours = Math.floor((distance % day) / hour)
     const minutes = Math.floor((distance % hour) / minute)
     const seconds = Math.floor((distance % minute) / second)
-    console.log(days,'days:', hours, 'hours:', minutes, 'minutes:', seconds,'seconds:');
-
-    // Populate Countdown
-    countdownElTitle.textContent = `${countdownTitle}`
-    timeElements[0].textContent = `${days}`
-    timeElements[1].textContent = `${hours}`
-    timeElements[2].textContent = `${minutes}`
-    timeElements[3].textContent = `${seconds}`
+    console.log(days, 'days:', hours, 'hours:', minutes, 'minutes:', seconds, 'seconds:')
 
 
     // Hide Input
     inputContainer.hidden = true
 
-    // Show Countdown
-    countdownEl.hidden = false
+    // If Countdown has ended
+    if (distance < 0) {
+      countdownEl.hidden = true
+      clearInterval(activeCountdown)
+      completeElInfo.textContent = `${countdownTitle} finished on ${countdownDate}`
+      completeEl.hidden = false
+    } else {
+      // Show the Countdown in progress
+      countdownElTitle.textContent = `${countdownTitle}`
+      timeElements[0].textContent = `${days}`
+      timeElements[1].textContent = `${hours}`
+      timeElements[2].textContent = `${minutes}`
+      timeElements[3].textContent = `${seconds}`
+      completeEl.hidden = true
+      countdownEl.hidden = false
+    }
+  }, second)
 }
 
+let activeCountdown;
 // function to take values from form input
 function updateCountdown(event) {
-    event.preventDefault();
-    const form = event.target;
-    countdownTitle = form[0].value;
-    const selectedDate = form[1].value;
-    const [year, month, day] = selectedDate.split('-');
-    countdownDate = `${month}/${day}/${year}`;
-    console.log(countdownTitle, countdownDate)
-    // Get Number Version of Current Date
-    countdownValue = new Date(countdownDate).getTime()
-    console.log('countdown value:', countdownValue)
-    startCountdown();
-  }
-  
-  
-  
+event.preventDefault();
+// Clear the previous countdown if it's still running
+clearInterval(activeCountdown);
+const countdownForm = event.target;
+countdownTitle = countdownForm[0].value;
+const selectedDate = countdownForm[1].value;
+const [year, month, day] = selectedDate.split('-');
+countdownDate = `${month}/${day}/${year}`;
+console.log(countdownTitle, countdownDate)
+// Check for Valid Date
+if (selectedDate === "") {
+alert('Please select a date for the Countdown')
+} else {
+// Get Number Version of Current Date
+countdownValue = new Date(countdownDate).getTime()
+console.log('countdown value:', countdownValue)
+startCountdown();
+}
+}
+
+// Reset values
+function reset() {
+// Hide countdown and show inputs
+countdownEl.hidden = true
+inputContainer.hidden = false
+// stop the countdown
+clearInterval(activeCountdown)
+// Reset values
+countdownForm[0].value = "";
+countdownForm[1].value = "";
+timeElements.forEach(timeElement => timeElement.textContent = "0");
+countdownTitle = "";
+countdownDate = "";
+return false;
+}
 
 // Event Listener
 countdownForm.addEventListener('submit', updateCountdown)
+countdownBtn.addEventListener('click', reset)
 
 
