77a601d0d174889f085c94924621200403004f00
hmustain <hunter@bunkerbranding.com>
2023-02-07 18:26:08 -0600

    saving work after defining and declaring startCountdown function

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
+
+
 let countdownTitle = "";
 let countdownDate = "";
 let countdownValue = Date;
@@ -12,6 +18,14 @@ const today = new Date().toLocaleDateString().split('T')[0];
 console.log(today)
 dateEl.setAttribute('min', today);
 
+// function for countdown
+function startCountdown() {
+    const now = new Date().getTime()
+    const distance = countdownValue - now
+    console.log('distance :', distance);
+}
+
+// function to take values from form input
 function updateCountdown(event) {
     event.preventDefault();
     const form = event.target;
@@ -23,6 +37,7 @@ function updateCountdown(event) {
     // Get Number Version of Current Date
     countdownValue = new Date(countdownDate).getTime()
     console.log('countdown value:', countdownValue)
+    startCountdown();
   }
   
   
